9253922d803dec5499752a16d3951a30e0f0585e
Michał Galus <galusmichal@outlook.com>
2023-09-01 00:28:39 +0200

    SCRUM-31 added Article JpaRepository

---
+++ src/main/java/pl/strefainformacji/repository/ArticleRepository.java
@@ -0,0 +1,7 @@
+package pl.strefainformacji.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArticleRepository extends JpaRepository<ArticleRepository, Long> {
+
+}
