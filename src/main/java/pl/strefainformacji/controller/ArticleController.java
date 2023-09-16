8346aa84736fb26dc8fdd890366108952dc04f81
Michał Galus <galusmichal@outlook.com>
2023-09-16 17:32:06 +0200

    SCRUM-31 delete some import in ArticleController

--- src/main/java/pl/strefainformacji/controller/ArticleController.java
+++ src/main/java/pl/strefainformacji/controller/ArticleController.java
@@ -5,10 +5,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import pl.strefainformacji.entity.ArticleInformation;
 import pl.strefainformacji.service.ArticleInformationService;
 
-import java.util.List;
 import java.util.NoSuchElementException;
 
 @RestController
