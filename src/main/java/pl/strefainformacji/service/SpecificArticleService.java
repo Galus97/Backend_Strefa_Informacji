ed5f4f1132856fcd8430b349e7dd0eed8722faf9
Michał Galus <galusmichal@outlook.com>
2023-10-03 23:30:52 +0200

    SCRUM-36 added implemetntatnion to SpecificArticleService

--- src/main/java/pl/strefainformacji/service/SpecificArticleService.java
+++ src/main/java/pl/strefainformacji/service/SpecificArticleService.java
@@ -5,13 +5,22 @@ import org.springframework.stereotype.Service;
 import pl.strefainformacji.entity.SpecificArticle;
 import pl.strefainformacji.repository.SpecificArticleRepository;
 
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
 @Service
 @AllArgsConstructor
 public class SpecificArticleService {
 
     private final SpecificArticleRepository specificArticleRepository;
 
-    public SpecificArticle getArticle(){
+    public SpecificArticle getArticle(Long number){
+        SpecificArticle article = specificArticleRepository.findByArticleInformation_Id(number);
 
+        if(Objects.isNull(article)){
+            throw new NoSuchElementException("There is no specific article in the database");
+        }
+
+        return article;
     }
 }
