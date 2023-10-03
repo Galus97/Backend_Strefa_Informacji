b8c50b3b99ebc9a587513dcf147d1eeb80dedc83
Michał Galus <galusmichal@outlook.com>
2023-10-03 23:14:48 +0200

    SCRUM-36 added SpecificArticleRepository

---
+++ src/main/java/pl/strefainformacji/repository/SpecificArticleRepository.java
@@ -0,0 +1,9 @@
+package pl.strefainformacji.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.strefainformacji.entity.SpecificArticle;
+
+public interface SpecificArticleRepository extends JpaRepository<SpecificArticle, Long> {
+
+
+}
