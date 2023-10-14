cb6fc78f1c598e8cc6c2f89113dbefa0f5f1a13b
Dawid <dawid.bartocha1997@gmail.com>
2023-10-14 18:21:37 +0200

    SCRUM-36_Deploy_Failing v15

--- src/main/java/pl/strefainformacji/configuration/CorsConfig.java
+++ src/main/java/pl/strefainformacji/configuration/CorsConfig.java
@@ -1,16 +1,20 @@
-package pl.strefainformacji.configuration;
-
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry){
-        registry.addMapping("/**")
-            .allowedOrigins("http://localhost:3000", "http://2.57.137.47", "http://dev.strefainformacji.pl")
-            .allowedMethods("GET", "POST", "PUT", "DELETE");
-    }
+    public class CorsConfig {
+        @Bean
+        public CorsFilter corsFilter() {
+            UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+            CorsConfiguration config = new CorsConfiguration();
+            config.addAllowedOrigin("http://localhost:3000");
+            config.addAllowedMethod("GET");
+            config.addAllowedHeader("Content-Type");
+            source.registerCorsConfiguration("/**", config);
+            return new CorsFilter((CorsConfigurationSource) source);
+        }
 }
