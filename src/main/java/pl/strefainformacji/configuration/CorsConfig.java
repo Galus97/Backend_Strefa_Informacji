013cd9e8b597597c729a45ed5ecaec6430d35ce4
Dawid <dawid.bartocha1997@gmail.com>
2023-10-14 17:05:17 +0200

    SCRUM-36_Deploy_Failing v11

--- src/main/java/pl/strefainformacji/configuration/CorsConfig.java
+++ src/main/java/pl/strefainformacji/configuration/CorsConfig.java
@@ -5,11 +5,13 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
+public class CorsConfig implements WebMvcConfigurerAdapter {
 
     @Override
     public void addCorsMappings(CorsRegistry registry){
-        registry.addMapping("/articles")
-                .allowedOrigins("http://localhost:3000", "http://2.57.137.47", "http://dev.strefainformacji.pl");
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:3000", "http://2.57.137.47", "http://dev.strefainformacji.pl")
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowCredentials(true);
     }
 }
