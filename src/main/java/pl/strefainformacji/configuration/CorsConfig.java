fc4b567f395ad42a235273a2ef682c1bd1d8c09b
Dawid <dawid.bartocha1997@gmail.com>
2023-10-14 18:28:30 +0200

    SCRUM-36_Deploy_Failing v16

--- src/main/java/pl/strefainformacji/configuration/CorsConfig.java
+++ src/main/java/pl/strefainformacji/configuration/CorsConfig.java
@@ -1,20 +1,17 @@
-import org.springframework.context.annotation.Bean;
+package pl.strefainformacji.configuration;
+
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-    public class CorsConfig {
-        @Bean
-        public CorsFilter corsFilter() {
-            UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-            CorsConfiguration config = new CorsConfiguration();
-            config.addAllowedOrigin("http://localhost:3000");
-            config.addAllowedMethod("GET");
-            config.addAllowedHeader("Content-Type");
-            source.registerCorsConfiguration("/**", config);
-            return new CorsFilter((CorsConfigurationSource) source);
-        }
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:3000", "http://2.57.137.47", "http://dev.strefainformacji.pl")
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowedHeaders("Content-Type");
+    }
 }
