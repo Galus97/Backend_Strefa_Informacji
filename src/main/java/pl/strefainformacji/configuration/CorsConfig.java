03507873299681c7a9cb1083b9b15147d393f7bb
Dawid <dawid.bartocha1997@gmail.com>
2023-10-18 11:04:31 +0200

    SCRUM-38 CORS Policy issue v4

--- src/main/java/pl/strefainformacji/configuration/CorsConfig.java
+++ src/main/java/pl/strefainformacji/configuration/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            // .allowedOrigins("http://localhost:3000", "http://2.57.137.47", "http://dev.strefainformacji.pl")
+            .allowedOrigins("http://localhost:3000", "http://2.57.137.47", "http://dev.strefainformacji.pl")
             .allowedMethods("GET", "POST", "PUT", "DELETE")
             .allowedHeaders("Content-Type")
             .allowCredentials(true);
