aacd01303e4c06259a21b5b0cb92ca9c52d81db5
Michał Galus <galusmichal@outlook.com>
2023-11-02 18:47:45 +0100

    SCRUM-42 come back to good value

--- src/main/java/pl/strefainformacji/controller/SpecificArticleController.java
+++ src/main/java/pl/strefainformacji/controller/SpecificArticleController.java
@@ -1,19 +1,13 @@
 package pl.strefainformacji.controller;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import pl.strefainformacji.entity.ArticleInformation;
-import pl.strefainformacji.service.ArticleInformationService;
 import pl.strefainformacji.service.SpecificArticleService;
 
-import java.util.List;
 import java.util.NoSuchElementException;
 
 @RestController
@@ -21,16 +15,13 @@ import java.util.NoSuchElementException;
 public class SpecificArticleController {
 
     private SpecificArticleService specificArticleService;
-    private final ArticleInformationService articleInformationService;
 
     @GetMapping("/article/{articleId}")
-    public ResponseEntity<?> getOneArticle(@PathVariable Long articleId, HttpServletResponse response){
-        HttpHeaders headers = new HttpHeaders();
-        try{
-            List<ArticleInformation> articles = articleInformationService.getAllArticles();
-            return ResponseEntity.ok(articleInformationService.getAllArticles());
-        } catch(NoSuchElementException exception){
+    public ResponseEntity<?> getOneArticle(@PathVariable Long articleId) {
+        try {
+            return ResponseEntity.ok(specificArticleService.getArticle(articleId));
+        } catch (NoSuchElementException exception) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
         }
-}
+    }
 }
