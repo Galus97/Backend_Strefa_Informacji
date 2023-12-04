7bdf84075fcf918171d58d95541540c7ad54dec0
Michał Galus <galusmichal@outlook.com>
2023-12-04 21:22:51 +0100

    SCRUM-42 added path to Amazon AWS to picture

--- src/main/java/pl/strefainformacji/controller/ImageViewController.java
+++ src/main/java/pl/strefainformacji/controller/ImageViewController.java
@@ -9,17 +9,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.net.URL;
 
 @RestController
 public class ImageViewController {
 
     @GetMapping("/image/{imageName}")
     public ResponseEntity<Resource> getImage (@PathVariable String imageName) throws Exception {
-        Path imagePath = Paths.get("/Users/michalgalus/Desktop/picture_backend/" + imageName + ".jpg");
+        String path = "https://s3.eu-north-1.amazonaws.com/strefainformacji.images/" + imageName + ".jpg";
 
-        Resource resource = new UrlResource(imagePath.toUri());
+        Resource resource = new UrlResource(new URL(path));
 
         return ResponseEntity.ok()
                 .header(HttpHeaders.CONTENT_TYPE, MediaType.IMAGE_JPEG_VALUE)
