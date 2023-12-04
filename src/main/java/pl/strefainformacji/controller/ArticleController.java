154d8d9d8fb9aabdaadc649bbf49b1a94171507b
Michał Galus <galusmichal@outlook.com>
2023-12-04 22:28:44 +0100

    SCRUM-42 added test for controllers

--- src/main/java/pl/strefainformacji/controller/ArticleController.java
+++ src/main/java/pl/strefainformacji/controller/ArticleController.java
@@ -1,7 +1,6 @@
 package pl.strefainformacji.controller;
 
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
