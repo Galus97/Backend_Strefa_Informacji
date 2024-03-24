d5c25192afa8b17d0ca076c39b4a02544dd9acda
Michał Galus <michalgalus@MacBook-Pro-Micha.local>
2024-03-24 22:21:20 +0100

    SCRUM-48_Added_send_mail

--- src/main/java/pl/strefainformacji/SpringStarter.java
+++ src/main/java/pl/strefainformacji/SpringStarter.java
@@ -1,11 +1,29 @@
 package pl.strefainformacji;
 
+import lombok.AllArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import pl.strefainformacji.service.EmailService;
+
+import java.util.Random;
 
 @SpringBootApplication
+@AllArgsConstructor
 public class SpringStarter {
+
+    private EmailService emailService;
     public static void main(String[] args) {
-        SpringApplication.run(SpringStarter.class, args);
+        ApplicationContext context = SpringApplication.run(SpringStarter.class, args);
+        SpringStarter app = context.getBean(SpringStarter.class);
+
+        app.Run();
+
+    }
+
+    private void Run(){
+        Random random = new Random();
+        random.nextInt(1000, 9999);
+        emailService.sendEmail("mgalus7@gmail.com", "Test mail subject", "Test mail body");
     }
 }
