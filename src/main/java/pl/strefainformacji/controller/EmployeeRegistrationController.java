f9dead73af391260cae3eae683fc38758c756da9
Michał Galus <michalgalus@MacBook-Pro-Micha.local>
2024-03-25 21:00:10 +0100

    SCRUM-48_Added_email_register

--- src/main/java/pl/strefainformacji/controller/EmployeeRegistrationController.java
+++ src/main/java/pl/strefainformacji/controller/EmployeeRegistrationController.java
@@ -1,6 +1,8 @@
 package pl.strefainformacji.controller;
 
+import jakarta.servlet.ServletContext;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -10,8 +12,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import pl.strefainformacji.entity.Employee;
 import pl.strefainformacji.exception.ValidationException;
+import pl.strefainformacji.service.EmailService;
 import pl.strefainformacji.service.RegistrationService;
 
+import java.util.Enumeration;
 import java.util.List;
 
 @Controller
@@ -19,6 +23,7 @@ import java.util.List;
 public class EmployeeRegistrationController {
 
     private final RegistrationService registrationService;
+    private final EmailService emailService;
 
     @GetMapping("/register")
     public String registerGet(Model model){
@@ -33,11 +38,18 @@ public class EmployeeRegistrationController {
         }
         try{
             registrationService.newEmployeeRegistration(employee);
+            sendActivationEmail(request, employee.getEmail());
             return "redirect:login";
         } catch (ValidationException exception){
             List<String> errors = exception.getValidationErrors();
             model.addAttribute("errors", errors);
             return "register";
         }
+    }
+
+    private void sendActivationEmail(HttpServletRequest request, String email){
+        HttpSession registerEmail = request.getSession();
+        registerEmail.setAttribute("registerEmail", email);
+        emailService.sendEmail();
     }
 }
