78165c7b40067cee346868444e0a57e6d351a4f8
Galus97 <102467635+Galus97@users.noreply.github.com>
2024-05-01 23:08:47 +0200

    Revert "Scrum 48 register login"

--- src/main/java/pl/strefainformacji/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package pl.strefainformacji.repository;
-
-import pl.strefainformacji.entity.Employee;
-
-import java.util.Optional;
-
-public interface EmployeeRepository {
-    Optional<Employee> findByUsername(String username);
-}
