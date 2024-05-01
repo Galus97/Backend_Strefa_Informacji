4314c1b3b45e9cd305ea92474db036055f445eee
Michał Galus <mgalus7@gmail.com>
2024-05-01 23:20:48 +0200

    SCRUM-48 added controllers and emailService

---
+++ src/main/java/pl/strefainformacji/controller/LoginEmployeeController.java
@@ -0,0 +1,14 @@
+package pl.strefainformacji.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@AllArgsConstructor
+public class LoginEmployeeController {
+    @GetMapping("/login")
+    public String loginGet(){
+        return "login";
+    }
+}
