d33f93f43d34f4b772ccd693a16722ec884bd8b3
Michał Galus <mgalus7@gmail.com>
2024-05-14 21:31:53 +0200

    SCRUM-48 fix csrf token

--- src/main/java/pl/strefainformacji/controller/ArticleImagesFormController.java
+++ src/main/java/pl/strefainformacji/controller/ArticleImagesFormController.java
@@ -1,35 +1,54 @@
 package pl.strefainformacji.controller;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import pl.strefainformacji.entity.ArticleImages;
+import pl.strefainformacji.entity.SpecificArticle;
 import pl.strefainformacji.service.ArticleImagesService;
 import pl.strefainformacji.service.SpecificArticleService;
+
+import java.util.Map;
+import java.util.Objects;
 
 @Controller
 @AllArgsConstructor
 public class ArticleImagesFormController {
     private final ArticleImagesService articleImagesService;
     private final SpecificArticleService specificArticleService;
+    private static final Logger logger = LoggerFactory.getLogger(ArticleImagesFormController.class);
+
     @GetMapping("/add/articleImages")
     public String articleImagesForm(@RequestParam(required = false)Long specificArticleId, HttpServletRequest request){
         request.setAttribute("specificArticleId", specificArticleId);
+        logger.info("1 -------------- articleImagesForm");
         return "articleImages";
     }
     @PostMapping("/add/articleImages")
-    public String saveArticleImagesFromForm(HttpServletRequest request){
+    public String saveArticleImagesFromForm(HttpServletRequest request, @RequestParam Map<String, String> allParams){
         Long specificArticleId = Long.parseLong(request.getParameter("specificArticleId"));
+        SpecificArticle specificArticleByArticleInformationId = specificArticleService.getSpecificArticleByArticleInformationId(specificArticleId);
+        System.out.println("------------------" + specificArticleId);
+        logger.info("Saving article images from form");
+
+        if(Objects.isNull(specificArticleId)){
+            logger.error("Specific article ID is null");
+            return "badBindingImages";
+        }
+
         for(int i = 1; i <= 10; i++){
-            String imgSrc = request.getParameter("imgSrc" + i);
-            String altImg = request.getParameter("altImg" + i);
-            if(!imgSrc.isEmpty() && !altImg.isEmpty()){
+            String imgSrc = allParams.get("imgSrc" + i);
+            String altImg = allParams.get("altImg" + i);
+
+            if(imgSrc != null && altImg != null && !imgSrc.isEmpty() && !altImg.isEmpty()){
                 ArticleImages articleImages = new ArticleImages();
                 articleImages.setImgSrc(imgSrc);
                 articleImages.setAltImg(altImg);
-                articleImages.setSpecificArticle(specificArticleService.getSpecificArticleByArticleInformationId(specificArticleId));
+                articleImages.setSpecificArticle(specificArticleByArticleInformationId);
                 articleImagesService.saveArticleImages(articleImages);
             }
         }
