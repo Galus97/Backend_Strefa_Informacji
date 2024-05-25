bbca1fdf65d4131612d2894e61167378e5168770
Michał Galus <mgalus7@gmail.com>
2024-05-25 16:08:20 +0200

    SCRUM-48 update

--- src/main/java/pl/strefainformacji/entity/ArticleInformation.java
+++ src/main/java/pl/strefainformacji/entity/ArticleInformation.java
@@ -35,19 +35,11 @@ public class ArticleInformation {
     @Max(10)
     private int importance;
 
-    @Lob
-    @Basic(fetch = FetchType.LAZY)
-    @Column(columnDefinition="blob")
-    private byte[] imageData;
-
     @Size(min = 1)
     private String imgSrc;
 
     @Size(min = 1)
     private String altImg;
 
-    public void setImageDataBase64(String base64){
-        this.imageData = Base64.getDecoder().decode(base64);
-    }
 
 }
