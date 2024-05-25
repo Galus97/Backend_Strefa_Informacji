0c9d72958c0d41069ebf4838d7be20680afa6409
Michał Galus <mgalus7@gmail.com>
2024-05-25 19:37:27 +0200

    SCRUM-48 fix SpecificArticle

--- src/main/java/pl/strefainformacji/entity/SpecificArticle.java
+++ src/main/java/pl/strefainformacji/entity/SpecificArticle.java
@@ -1,5 +1,6 @@
 package pl.strefainformacji.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
