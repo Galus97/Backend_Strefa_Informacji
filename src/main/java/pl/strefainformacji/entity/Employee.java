71d5559ac0310e7aeb8a64a2e3be1a874bbd25d8
Michał Galus <mgalus7@gmail.com>
2024-06-28 23:15:17 +0200

    SCRUM-49 added new filed to Employee

---
+++ src/main/java/pl/strefainformacji/entity/Employee.java
@@ -0,0 +1,36 @@
+package pl.strefainformacji.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Entity
+@Table (name = "employees")
+@Getter
+@Setter
+@ToString
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long employeeId;
+
+    @Size(min = 3)
+    private String firstName;
+
+    @Size(min = 3)
+    private String lastName;
+
+    @Size(min = 3)
+    private String email;
+
+    @Size(min = 3)
+    private String username;
+
+    @Size(min = 5)
+    private String password;
+
+    private boolean enabled;
+
+    private int verifyEmailCode;
+}
