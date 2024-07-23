c3396978991c72ee07e551e93c5430011885aad1
Michał Galus <mgalus7@gmail.com>
2024-07-23 13:22:40 +0200

    SCRUM-50 delete apiKey from class

--- src/main/java/pl/strefainformacji/webclient/weather/WeatherClient.java
+++ src/main/java/pl/strefainformacji/webclient/weather/WeatherClient.java
@@ -1,5 +1,6 @@
 package pl.strefainformacji.webclient.weather;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import pl.strefainformacji.model.WeatherDto;
@@ -8,13 +9,14 @@ import pl.strefainformacji.webclient.weather.dto.OpenWeatherDto;
 @Component
 public class WeatherClient {
     private static final String WEATER_URL = "https://api.openweathermap.org/data/2.5/";
-    private static final String API_KEY = "104430623b731b9a297d4421d09703ce";
+    @Value("${weather.api.key}")
+    private String apiKey;
     private RestTemplate restTemplate = new RestTemplate();
 
     public WeatherDto getWeatherForCity(double lat, double lon) {
         OpenWeatherDto openWeatherDto = restTemplate.getForObject(
                 WEATER_URL + "weather?lat={lat}&lon={lon}&appid={apiKey}&units=metric&lang=pl",
-                OpenWeatherDto.class, lat, lon, API_KEY);
+                OpenWeatherDto.class, lat, lon, apiKey);
         return WeatherDto.builder()
                 .temperatue(openWeatherDto.getMain().getTemp())
                 .pressure(openWeatherDto.getMain().getPressure())
