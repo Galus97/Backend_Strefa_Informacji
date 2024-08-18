e98d8ea228c6f9cd95a266b908ed953a684cc15d
Michał Galus <mgalus7@gmail.com>
2024-08-18 13:03:36 +0200

    SCRUM-54 added SaveArticleController

---
+++ src/main/java/pl/strefainformacji/controller/SaveArticleController.java
@@ -0,0 +1,71 @@
+package pl.strefainformacji.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import pl.strefainformacji.component.CurrentEmployee;
+import pl.strefainformacji.entity.ArticleImages;
+import pl.strefainformacji.entity.ArticleInformation;
+import pl.strefainformacji.entity.SpecificArticle;
+import pl.strefainformacji.service.ArticleImagesService;
+import pl.strefainformacji.service.ArticleInformationService;
+import pl.strefainformacji.service.SpecificArticleService;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+@Controller
+@RequiredArgsConstructor
+public class SaveArticleController {
+    private final ArticleInformationService articleInformationService;
+    private final SpecificArticleService specificArticleService;
+    private final ArticleImagesService articleImagesService;
+    private final ArticleInformationFormController articleInformationFormController;
+    private final SpecificArticleFormController specificArticleFormController;
+    private final ArticleImagesFormController articleImagesFormController;
+
+    @GetMapping("/article")
+    public String getArticle(@AuthenticationPrincipal CurrentEmployee curentEmployee) {
+
+        ArticleInformation articleInformation = articleInformationFormController.articleInformation;
+        SpecificArticle specificArticle = specificArticleFormController.specificArticle;
+        List<ArticleImages> articleImagesList = articleImagesFormController.articleImagesList;
+
+        articleInformation.setEmployee(curentEmployee.getEmployee());
+        articleInformation.setContentfulId("00000");
+        specificArticle.setArticleInformation(articleInformationFormController.articleInformation);
+        for (ArticleImages articleImages : articleImagesList) {
+            articleImages.setSpecificArticle(specificArticleFormController.specificArticle);
+        }
+
+        if (isEveryFieldsExist(articleInformation, specificArticle, articleImagesList)) {
+            articleInformationService.saveArticle(articleInformation);
+            specificArticleService.saveSpecificArticle(specificArticle);
+            for (ArticleImages articleImages : articleImagesList) {
+                articleImagesService.saveArticleImages(articleImages);
+            }
+        }
+        return "article";
+    }
+
+    private boolean isEveryFieldsExist(ArticleInformation articleInformation, SpecificArticle specificArticle, List<ArticleImages> articleImages) {
+
+        if (articleInformation.getContentfulId() == null || articleInformation.getTitle() == null || articleInformation.getShortDescription() == null ||
+                articleInformation.getImportance() == null || articleInformation.getImgSrc() == null || articleInformation.getAltImg() == null || articleInformation.getEmployee() == null) {
+            return false;
+        }
+
+        if (specificArticle.getTitle() == null || specificArticle.getDescription() == null || specificArticle.getArticleInformation() == null) {
+            return false;
+        }
+
+        for (ArticleImages articleImage : articleImages) {
+            if (articleImage.getImgSrc() == null || articleImage.getAltImg() == null || articleImage.getSpecificArticle() == null) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
