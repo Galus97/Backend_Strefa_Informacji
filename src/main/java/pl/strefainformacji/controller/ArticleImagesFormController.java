c92a56ed08d88a62d913f5b8daac9f6c57560fcd
Michał Galus <mgalus7@gmail.com>
2024-08-18 13:02:34 +0200

    SCRUM-54 change save article system to save in last step

--- src/main/java/pl/strefainformacji/controller/ArticleImagesFormController.java
+++ src/main/java/pl/strefainformacji/controller/ArticleImagesFormController.java
@@ -1,6 +1,7 @@
 package pl.strefainformacji.controller;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -15,19 +16,22 @@ import pl.strefainformacji.service.ArticleImagesService;
 import pl.strefainformacji.service.EmployeeService;
 import pl.strefainformacji.service.SpecificArticleService;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @Controller
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class ArticleImagesFormController {
     private final ArticleImagesService articleImagesService;
     private final SpecificArticleService specificArticleService;
     private final EmployeeService employeeService;
-    private static final Logger logger = LoggerFactory.getLogger(ArticleImagesFormController.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(ArticleImagesFormController.class);
+
+    public List<ArticleImages> articleImagesList;
 
     @GetMapping("/add/articleImages")
-    public String articleImagesForm(@RequestParam(required = false)Long specificArticleId, HttpServletRequest request, @AuthenticationPrincipal CurrentEmployee curentEmployee){
-        request.setAttribute("specificArticleId", specificArticleId);
+    public String articleImagesForm(@AuthenticationPrincipal CurrentEmployee curentEmployee){
         if (employeeService.isEnabledById(curentEmployee.getEmployee().getEmployeeId())) {
             return "articleImages";
         } else {
@@ -36,30 +40,9 @@ public class ArticleImagesFormController {
 
     }
     @PostMapping("/add/articleImages")
-    public String saveArticleImagesFromForm(HttpServletRequest request, @RequestParam Map<String, String> allParams){
-        String specificArticleIdStr = request.getParameter("specificArticleId");
-        if (specificArticleIdStr == null) {
-            logger.error("Specific article ID is null");
-            return "badBindingImages";
-        }
-        Long specificArticleId;
+    public String saveArticleImagesFromForm(@RequestParam Map<String, String> allParams){
 
-        try{
-            specificArticleId = Long.parseLong(specificArticleIdStr);
-        } catch (NumberFormatException e){
-            logger.error("Specific article ID is invalid: " + specificArticleIdStr);
-            return "badBindingImages";
-        }
-
-
-        SpecificArticle specificArticleByArticleInformationId = specificArticleService.getSpecificArticle(specificArticleId);
-        logger.info("Saving article images from form");
-
-        if(specificArticleByArticleInformationId == null){
-            logger.error("Specific article ID is null");
-            return "badBindingImages";
-        }
-
+        articleImagesList = new ArrayList<>();
         for(int i = 1; i <= 10; i++){
             String imgSrc = allParams.get("imgSrc" + i);
             String altImg = allParams.get("altImg" + i);
@@ -68,10 +51,11 @@ public class ArticleImagesFormController {
                 ArticleImages articleImages = new ArticleImages();
                 articleImages.setImgSrc(imgSrc);
                 articleImages.setAltImg(altImg);
-                articleImages.setSpecificArticle(specificArticleByArticleInformationId);
-                articleImagesService.saveArticleImages(articleImages);
+                articleImagesList.add(articleImages);
+                //articleImagesService.saveArticleImages(articleImages);
             }
         }
-        return "redirect:viewAddedArticle?specificArticleId=" + specificArticleId;
+        //return "redirect:viewAddedArticle?specificArticleId=" + specificArticleId;
+        return "redirect:/article";
     }
 }
