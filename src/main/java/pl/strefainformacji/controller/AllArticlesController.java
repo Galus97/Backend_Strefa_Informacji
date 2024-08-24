14e340b80d73748229d604517c03b64e1391bf95
Michał Galus <mgalus7@gmail.com>
2024-08-24 17:23:37 +0200

    SCRUM-54 change AllArticlesController to view jsp file

--- src/main/java/pl/strefainformacji/controller/AllArticlesController.java
+++ src/main/java/pl/strefainformacji/controller/AllArticlesController.java
@@ -1,26 +1,35 @@
 package pl.strefainformacji.controller;
 
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.strefainformacji.component.CurrentEmployee;
 import pl.strefainformacji.service.ArticleInformationService;
+import pl.strefainformacji.service.EmployeeService;
 
 import java.util.NoSuchElementException;
 
-@RestController
-@AllArgsConstructor
+@Controller
+@RequiredArgsConstructor
 public class AllArticlesController {
 
 
     private final ArticleInformationService articleInformationService;
+    private final EmployeeService employeeService;
+
     @GetMapping("/articles")
-    public ResponseEntity<?> getAllArticles(){
-        try{
-            return ResponseEntity.ok(articleInformationService.getAllArticles());
-        } catch(NoSuchElementException exception){
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
+    public String getAllArticles(@AuthenticationPrincipal CurrentEmployee curentEmployee, Model model) {
+        if (employeeService.isEnabledById(curentEmployee.getEmployee().getEmployeeId())) {
+            model.addAttribute("allArticles", articleInformationService.getAllArticles());
+            return "allArticles";
+        } else {
+            return "redirect:verifyEmail";
         }
     }
 }
