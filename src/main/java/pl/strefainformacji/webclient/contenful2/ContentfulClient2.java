671e5e1e927c8447982568286014223ef77405ed
Michał Galus <mgalus7@gmail.com>
2024-09-06 23:30:00 +0200

    SCRUM-55 added ContentfulClient with CDAClient

--- src/main/java/pl/strefainformacji/webclient/contenful2/ContentfulClient2.java
+++ src/main/java/pl/strefainformacji/webclient/contenful2/ContentfulClient2.java
@@ -1,16 +1,20 @@
-package pl.strefainformacji.webclient.contentful;
+package pl.strefainformacji.webclient.contenful2;
 
 import com.contentful.java.cda.CDAClient;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ContentfulClient2 {
 
-    @Value("${contentful.access.token}")
-    private static String ACCESS_TOKEN;
-    private static final String SPACE_ID = "wrhey748z2h3";
+    @Value("${access.token.contentful}")
+    private String ACCESS_TOKEN;
+
+    @Value("${space.id}")
+    private String SPACE_ID;
 
 
-    public static CDAClient createClient(){
+    public CDAClient createClient(){
         return CDAClient.builder()
                 .setSpace(SPACE_ID)
                 .setToken(ACCESS_TOKEN)
