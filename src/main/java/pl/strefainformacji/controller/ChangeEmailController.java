6bfaaede79094c03fe8e7565fe92f1e68d8927ce
Michał Galus <mgalus7@gmail.com>
2024-10-04 21:13:40 +0200

    SCRUM-57 added postMaping to ChangeEmailController

--- src/main/java/pl/strefainformacji/controller/ChangeEmailController.java
+++ src/main/java/pl/strefainformacji/controller/ChangeEmailController.java
@@ -1,9 +1,11 @@
 package pl.strefainformacji.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import pl.strefainformacji.component.CurrentEmployee;
 import pl.strefainformacji.service.EmployeeService;
 
@@ -19,5 +21,23 @@ public class ChangeEmailController {
         } else {
             return "redirect:verifyEmail";
         }
+    }
+
+    @PostMapping("/changeEmail")
+    public String changeEmailPost(@AuthenticationPrincipal CurrentEmployee curentEmployee, HttpServletRequest request) {
+        String newEmail = request.getParameter("newEmail");
+        String newEmailAgain = request.getParameter("newEmailAgain");
+
+        if (!newEmail.equals(newEmailAgain)) {
+            return "changeEmail";
+        }
+
+        if (newEmail.equals(curentEmployee.getEmployee().getEmail())) {
+            return "changeEmail";
+        }
+
+        employeeService.changeEmail(curentEmployee.getEmployee().getEmployeeId(), newEmail);
+        curentEmployee.getEmployee().setEmail(newEmail);
+        return "redirect:panel";
     }
 }
