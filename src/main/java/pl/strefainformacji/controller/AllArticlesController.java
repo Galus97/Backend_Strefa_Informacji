f511452feb2f264bfea16ba9cd6f03340463e556
michalgalus <galusmichal@outlook.com>
2025-03-04 17:16:43 +0100

    SCRUM-57 added @RequestMapping to AllArticlesController

--- src/main/java/pl/strefainformacji/controller/AllArticlesController.java
+++ src/main/java/pl/strefainformacji/controller/AllArticlesController.java
@@ -5,18 +5,20 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import pl.strefainformacji.component.CurrentEmployee;
 import pl.strefainformacji.service.ArticleInformationService;
 import pl.strefainformacji.service.EmployeeService;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/article")
 public class AllArticlesController {
 
     private final ArticleInformationService articleInformationService;
     private final EmployeeService employeeService;
 
-    @GetMapping("/articles")
+    @GetMapping("/all")
     public String showAllArticles(@AuthenticationPrincipal CurrentEmployee currentEmployee, Model model) {
         if (employeeService.isEnabledById(currentEmployee.getEmployee().getEmployeeId())) {
             model.addAttribute("allArticles", articleInformationService.getAllArticles());
