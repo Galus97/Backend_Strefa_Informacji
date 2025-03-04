e34774c9cdc15d4d2edc5be4d4b8312f4af626ab
michalgalus <galusmichal@outlook.com>
2025-03-04 09:01:04 +0100

    SCRUM-57 added MessageService.java

---
+++ src/main/java/pl/strefainformacji/component/MessageService.java
@@ -0,0 +1,9 @@
+package pl.strefainformacji.component;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class MessageService {
+}
