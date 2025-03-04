ac7f3fac24a0b5ba0c41387576bcbb63d4c5ba2b
michalgalus <galusmichal@outlook.com>
2025-03-04 16:30:18 +0100

    SCRUM-57 added isValid to ArticleDto

--- src/main/java/pl/strefainformacji/model/ArticleDto.java
+++ src/main/java/pl/strefainformacji/model/ArticleDto.java
@@ -15,4 +15,27 @@ public class ArticleDto {
     private ArticleInformation articleInformation;
     private SpecificArticle specificArticle;
     private List<ArticleImages> images = new ArrayList<>();
+
+    public boolean isValid() {
+        if (articleInformation == null || specificArticle == null || images == null) {
+            return false;
+        }
+
+        boolean isInformationValid =
+                articleInformation.getTitle() != null && !articleInformation.getTitle().isBlank() &&
+                        articleInformation.getShortDescription() != null && !articleInformation.getShortDescription().isBlank() &&
+                        articleInformation.getImportance() != null;
+
+        boolean isSpecificValid =
+                specificArticle.getTitle() != null && !specificArticle.getTitle().isBlank() &&
+                        specificArticle.getDescription() != null && !specificArticle.getDescription().isBlank();
+
+        boolean areImagesValid = !images.isEmpty() &&
+                images.stream().allMatch(img ->
+                        img.getImgSrc() != null && !img.getImgSrc().isBlank() &&
+                                img.getAltImg() != null && !img.getAltImg().isBlank()
+                );
+
+        return isInformationValid && isSpecificValid && areImagesValid;
+    }
 }
