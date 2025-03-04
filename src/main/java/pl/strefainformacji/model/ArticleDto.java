1e0827bc982773e948623ad15f34a12b7694e05c
michalgalus <galusmichal@outlook.com>
2025-03-04 16:02:50 +0100

    SCRUM-57 added fields to ArticleDto

--- src/main/java/pl/strefainformacji/model/ArticleDto.java
+++ src/main/java/pl/strefainformacji/model/ArticleDto.java
@@ -1,4 +1,18 @@
 package pl.strefainformacji.model;
 
+import lombok.Getter;
+import lombok.Setter;
+import pl.strefainformacji.entity.ArticleImages;
+import pl.strefainformacji.entity.ArticleInformation;
+import pl.strefainformacji.entity.SpecificArticle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
 public class ArticleDto {
+    private ArticleInformation articleInformation;
+    private SpecificArticle specificArticle;
+    private List<ArticleImages> images = new ArrayList<>();
 }
