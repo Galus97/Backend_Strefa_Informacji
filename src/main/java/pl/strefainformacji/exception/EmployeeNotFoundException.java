5163dd97e2d27f2dcb208b58694afc9080a965a6
michalgalus <galusmichal@outlook.com>
2025-03-04 11:01:10 +0100

    SCRUM-57 added EmployeeNotFoundException.java

---
+++ src/main/java/pl/strefainformacji/exception/EmployeeNotFoundException.java
@@ -0,0 +1,7 @@
+package pl.strefainformacji.exception;
+
+public class EmployeeNotFoundException extends RuntimeException {
+    public EmployeeNotFoundException(String message) {
+        super(message);
+    }
+}
