8ab2cc6215c34de90512856b5131524b27facd23
michalgalus <galusmichal@outlook.com>
2025-03-04 09:29:32 +0100

    SCRUM-57 added documentation to CorsConfig

--- src/main/java/pl/strefainformacji/configuration/CorsConfig.java
+++ src/main/java/pl/strefainformacji/configuration/CorsConfig.java
@@ -4,9 +4,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * Configuration class for setting up Cross-Origin Resource Sharing (CORS) in the application.
+ */
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
+    /**
+     * Configures CORS mappings to allow requests from specified origins, methods, and headers.
+     *
+     * @param registry The {@link CorsRegistry} used to configure CORS settings.
+     */
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
