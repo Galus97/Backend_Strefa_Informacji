a725b66d97219d9473417cd10c37205ed1059b0f
michalgalus <galusmichal@outlook.com>
2025-03-06 23:24:55 +0100

    SCRUM-57 added ContentfulIntegrationException.java

---
+++ src/main/java/pl/strefainformacji/exception/ContentfulIntegrationException.java
@@ -0,0 +1,7 @@
+package pl.strefainformacji.exception;
+
+public class ContentfulIntegrationException extends RuntimeException {
+    public ContentfulIntegrationException(String message) {
+        super(message);
+    }
+}
