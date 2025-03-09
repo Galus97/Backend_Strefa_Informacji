5afc61058651f7f1347896519cf493b1b10cf6bd
michalgalus <galusmichal@outlook.com>
2025-03-09 23:35:40 +0100

    SCRUM-57 added fields to Fields class inside ContentfulArticleDto

--- src/main/java/pl/strefainformacji/webclient/contentful/dto/ContentfulArticleDto.java
+++ src/main/java/pl/strefainformacji/webclient/contentful/dto/ContentfulArticleDto.java
@@ -1,54 +1,39 @@
 package pl.strefainformacji.webclient.contentful.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
 import java.util.List;
 
 @Data
-@JsonIgnoreProperties(ignoreUnknown = true)
+@Accessors(chain = true)
 public class ContentfulArticleDto {
-    private Fields fields;
     private Sys sys;
+    private Fields fields;
 
     @Data
     @Accessors(chain = true)
     public static class Sys {
-        @JsonProperty("id")
         private String id;
     }
 
     @Data
     @Accessors(chain = true)
     public static class Fields {
-        @JsonProperty("headTitle")
         private String headTitle;
-        @JsonProperty("shortDescription")
         private String shortDescription;
-        @JsonProperty("importance")
         private Integer importance;
-        @JsonProperty("headImgSrc")
         private Sys headImgSrc;
-        @JsonProperty("headAltImg")
-        private String headAltImg;
-        @JsonProperty("specificTitle")
-        private String specificTitle;
-        @JsonProperty("description")
-        private String description;
-        @JsonProperty("imgSrc")
         private List<Sys> imgSrcList;
-        @JsonIgnore
         private List<String> altImgList;
-        @JsonProperty("employeeId")
         private Integer employeeId;
+        private String headAltImg;
+        private String specificTitle;
+        private String description;
 
         @Data
         @Accessors(chain = true)
         public static class Sys {
-            @JsonProperty("id")
             private String id;
         }
     }
