15e4048824d947de6c489dc5d8be6926d318f367
michalgalus <galusmichal@outlook.com>
2025-03-15 16:14:45 +0100

    SCRUM-58 used ErrorMessages in ArticleImagesService

--- src/main/java/pl/strefainformacji/service/ArticleImagesService.java
+++ src/main/java/pl/strefainformacji/service/ArticleImagesService.java
@@ -2,6 +2,7 @@ package pl.strefainformacji.service;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import pl.strefainformacji.component.ErrorMessages;
 import pl.strefainformacji.component.MessageService;
 import pl.strefainformacji.entity.ArticleImages;
 import pl.strefainformacji.entity.SpecificArticle;
@@ -14,20 +15,16 @@ import java.util.Objects;
 @Service
 @AllArgsConstructor
 public class ArticleImagesService {
-    public static final String ARTICLE_IMAGES_IS_NULL = "error.articleImagesIsNull";
-    public static final String SPECIFIC_ARTICLE_IS_NULL = "error.specificArticleIsNull";
-    public static final String ARTICLE_IMAGES_NOT_FOUND = "error.articleImagesNotFound";
-
     private final ArticleImagesRepository articleImagesRepository;
     private final MessageService messageService;
 
     public void saveArticleImages(ArticleImages articleImages) {
-        throwIfObjectIsNull(articleImages, ARTICLE_IMAGES_IS_NULL);
+        throwIfObjectIsNull(articleImages, ErrorMessages.ARTICLE_IMAGES_IS_NULL);
         articleImagesRepository.save(articleImages);
     }
 
     public List<ArticleImages> getAllArticleImagesBySpecificArticle(SpecificArticle specificArticle) {
-        throwIfObjectIsNull(specificArticle, SPECIFIC_ARTICLE_IS_NULL);
+        throwIfObjectIsNull(specificArticle, ErrorMessages.SPECIFIC_ARTICLE_IS_NULL);
 
         throwIfImagesNotFound(specificArticle.getSpecificArticleId());
 
@@ -36,7 +33,7 @@ public class ArticleImagesService {
 
     public void throwIfImagesNotFound(Long id) {
         if (!articleImagesRepository.existsBySpecificArticle_SpecificArticleId(id)) {
-            throw new ArticleImagesNotFoundException(messageService.getMessage(ARTICLE_IMAGES_NOT_FOUND, id));
+            throw new ArticleImagesNotFoundException(messageService.getMessage(ErrorMessages.ARTICLE_IMAGES_NOT_FOUND, id));
         }
     }
 
