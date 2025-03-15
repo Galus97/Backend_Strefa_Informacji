1567ab460e25e9a114b845f4385bb099a4b25687
michalgalus <galusmichal@outlook.com>
2025-03-15 16:14:22 +0100

    SCRUM-58 added new fields in ErrorMessages

--- src/main/java/pl/strefainformacji/component/ErrorMessages.java
+++ src/main/java/pl/strefainformacji/component/ErrorMessages.java
@@ -10,4 +10,6 @@ public final class ErrorMessages {
     public static final String SPECIFIC_ARTICLE_NOT_FOUND = "error.invalidArticleId";
     public static final String SPECIFIC_ARTICLE_IS_NULL = "error.specificArticleIsNull";
     public static final String INVALID_EMAIL = "error.invalidEmailAddress";
+    public static final String ARTICLE_IMAGES_IS_NULL = "error.articleImagesIsNull";
+    public static final String ARTICLE_IMAGES_NOT_FOUND = "error.articleImagesNotFound";
 }
