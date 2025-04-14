e9d1979022a28c2c2f739e9a7d2545f6dd0294d4
michalgalus <galusmichal@outlook.com>
2025-04-14 11:46:41 +0200

    SCRUM-58 added handleSpecificArticleNotFoundException to GlobalExceptionHandler

--- src/main/java/pl/strefainformacji/exception/GlobalExceptionHandler.java
+++ src/main/java/pl/strefainformacji/exception/GlobalExceptionHandler.java
@@ -36,6 +36,11 @@ public class GlobalExceptionHandler {
         return getMapResponseEntity(e);
     }
 
+    @ExceptionHandler(SpecificArticleNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handleSpecificArticleNotFoundException(SpecificArticleNotFoundException e) {
+        return getMapResponseEntity(e);
+    }
+
     private static ResponseEntity<Map<String, String>> getMapResponseEntity(RuntimeException e) {
         Map<String, String> response = new HashMap<>();
         response.put(ErrorMessages.ERROR, e.getMessage());
