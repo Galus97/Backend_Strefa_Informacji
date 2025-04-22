b5a1e4647ed6dc8a5c97ce357e5314364e82abdc
michalgalus <galusmichal@outlook.com>
2025-04-22 13:38:30 +0200

    SCRUM-58 added setUp to  SaveArticleControllerTest

--- src/test/java/pl/strefainformacji/controller/save/SaveArticleControllerTest.java
+++ src/test/java/pl/strefainformacji/controller/save/SaveArticleControllerTest.java
@@ -1,5 +1,40 @@
 package pl.strefainformacji.controller.save;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.security.authentication.TestingAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.test.web.servlet.MockMvc;
+import pl.strefainformacji.model.ArticleDto;
+import pl.strefainformacji.service.ArticleService;
+
+import java.util.Collections;
+
+@WebMvcTest(SaveArticleController.class)
 class SaveArticleControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ArticleService articleService;
+
+    private ArticleDto mockArticleDto;
+    private MockHttpSession session;
+    private TestingAuthenticationToken authToken;
+
+    @BeforeEach
+    void setup() {
+        authToken = new TestingAuthenticationToken("user", "pass", Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER")));
+        authToken.setAuthenticated(true);
+
+        mockArticleDto = org.mockito.Mockito.mock(ArticleDto.class);
+        session = new MockHttpSession();
+        session.setAttribute("articleDto", mockArticleDto);
+    }
+
+    
 }
