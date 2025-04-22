db0dc3496750247360adc09a18bf1adbdc6992f7
michalgalus <galusmichal@outlook.com>
2025-04-22 13:30:50 +0200

    SCRUM-58 added SpecificArticleFormControllerTest.java

---
+++ src/test/java/pl/strefainformacji/controller/save/SpecificArticleFormControllerTest.java
@@ -0,0 +1,9 @@
+package pl.strefainformacji.controller.save;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest(SpecificArticleFormController.class)
+class SpecificArticleFormControllerTest {
+
+    
+}
