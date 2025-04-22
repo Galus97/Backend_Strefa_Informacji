3d84160fc602537d1e218cd3e6eae82eab8f7277
michalgalus <galusmichal@outlook.com>
2025-04-22 13:32:00 +0200

    SCRUM-58 added setup to SpecificArticleFormControllerTest

--- src/test/java/pl/strefainformacji/controller/save/SpecificArticleFormControllerTest.java
+++ src/test/java/pl/strefainformacji/controller/save/SpecificArticleFormControllerTest.java
@@ -1,9 +1,47 @@
 package pl.strefainformacji.controller.save;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.authentication.TestingAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.test.web.servlet.MockMvc;
+import pl.strefainformacji.component.CurrentEmployee;
+import pl.strefainformacji.entity.Employee;
+import pl.strefainformacji.service.EmployeeService;
+
+import java.util.Collections;
 
 @WebMvcTest(SpecificArticleFormController.class)
 class SpecificArticleFormControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private EmployeeService employeeService;
+
+    private CurrentEmployee currentEmployee;
+    private TestingAuthenticationToken authToken;
+
+    @BeforeEach
+    void setup() {
+        Employee emp = new Employee();
+        emp.setEmployeeId(100L);
+        currentEmployee = new CurrentEmployee(
+                "user",
+                "pass",
+                Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER")),
+                emp
+        );
+        authToken = new TestingAuthenticationToken(
+                currentEmployee,
+                currentEmployee.getPassword(),
+                currentEmployee.getAuthorities()
+        );
+        authToken.setAuthenticated(true);
+    }
 
     
 }
