9a5a2171e83926362d8e79475b1ea660ccbc8e4d
michalgalus <galusmichal@outlook.com>
2025-04-27 18:34:02 +0200

    SCRUM-58 added RequestMapping to ArticleImagesFormController

--- src/main/java/pl/strefainformacji/controller/save/ArticleImagesFormController.java
+++ src/main/java/pl/strefainformacji/controller/save/ArticleImagesFormController.java
@@ -10,6 +10,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import pl.strefainformacji.component.CurrentEmployee;
 import pl.strefainformacji.component.MessageService;
 import pl.strefainformacji.entity.ArticleImages;
@@ -25,12 +26,13 @@ import java.util.stream.Collectors;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/add")
 public class ArticleImagesFormController {
     private static final String ARTICLE_IMAGES_PAGE = "articleImages";
     private final EmployeeService employeeService;
     private final MessageService messageService;
 
-    @GetMapping("/add/articleImages")
+    @GetMapping("/articleImages")
     public String showArticleImagesForm(@AuthenticationPrincipal CurrentEmployee currentEmployee, Model model, HttpSession session) {
         if (employeeService.isEnabledById(currentEmployee.getEmployee().getEmployeeId())) {
             ArticleImagesForm form = (ArticleImagesForm) session.getAttribute("articleImagesForm");
@@ -45,7 +47,7 @@ public class ArticleImagesFormController {
         }
     }
 
-    @PostMapping("/add/articleImages")
+    @PostMapping("/articleImages")
     public String saveArticleImages(@Valid @ModelAttribute("form") ArticleImagesForm form, BindingResult bindingResult,
                                     HttpSession session, Locale locale) {
 
